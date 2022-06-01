b7c0a8921a5b0f6220676e2157f320bdf0a8e8ab
LukeDWaller99 <lukedwaller99@gmail.com>
2022-06-01 13:05:27 +0100

    Add constructor to classes

--- nRF24L01 Library/nRF24L01/nRF24L01.cpp
+++ nRF24L01 Library/nRF24L01/nRF24L01.cpp
@@ -1,1 +1,21 @@
+/**
+ * @file nRF24L01P.cpp
+ *
+ */
+
+/**
+ * Includes
+ */
 #include <nRF24L01.h>
+
+/**
+ * Method
+ */
+
+nRF24L01::nRF24L01(PinName mosi,
+                   PinName miso,
+                   PinName clk,
+                   PinName csn,
+                   PinName ce,
+                   PinName irq) : _spi(mosi, miso, clk), _csn(csn), _ce(ce), _irq(irq){
+                   };
--- nRF24L01 Library/nRF24L01/nRF24L01.h
+++ nRF24L01 Library/nRF24L01/nRF24L01.h
@@ -1,5 +1,37 @@
+/**
+ * @file nRF24L01P.h
+ */
+
 #ifndef __nRF24L01_H__
 #define __nRF24L01_H__
 
+/**
+ * Includes
+ */
+#include "PinNames.h"
+#include "mbed.h"
+
+class nRF24L01{
+
+    public:
+    /**
+     * Constructor
+     *
+     * @param mosi
+     * @param miso
+     * @param clk
+     * @param csn
+     * @param ce
+     * @param irq
+     */
+    nRF24L01(PinName mosi, PinName miso, PinName clk, PinName csn, PinName ce, PinName irq = NC);
+
+    private:
+
+    SPI         _spi;
+    DigitalOut  _ce, _csn;
+    InterruptIn _irq;
+
+};
 
 #endif
